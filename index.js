4a78fac40424fb17701a3a849d04225abf0bbf27
elweondelabolsa <50603955+elweondelabosa@users.noreply.github.com>
2019-11-04 20:12:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// dependencias
+
 const express = require('express');
 const mongoose = require('mongoose');
 
-// config
+
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/trivia';
 
-// objeto app
+
 const app = express();
 
-// conectarse a la base de datos
+
 mongoose.set('useFindAndModify', false);
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -18,22 +18,22 @@ mongoose
   })
 .catch(err => console.error(`Connection error ${err}`));
 
-// schema y modelo de pregunta
+
 const QuestionSchema = new mongoose.Schema({
   question: { type: String, required: true }
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
 
-// middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// config de vistas
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-// rutas
+
 app.post('/question', (req, res) => {
   let question = new Question({
     question: req.body.question
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
   });
 });
 
-// listen
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
